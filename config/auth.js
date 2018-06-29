2c64b72098cc8e504ec605d83c675eca068612a8
Matthew Paulino <matthewpaulino@Matthews-MacBook-Pro.local>
2018-06-28 18:53:05 -0700

    initial

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+  twitterAuth: {
+  'consumerKey': 'LcjhjBu5hKKMtr95fGoE5mPEI',
+  'consumerSecret':	'3K4QNZAkplJ02Ks53iod89D6gisora0L7lt9ol6PQklbbojKbk',
+  'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+  }
+};
+
+
+
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url': 'mongodb://coders_bay:UCIcodecamp1@ds117701.mlab.com:17701/coders_bay'
+}
